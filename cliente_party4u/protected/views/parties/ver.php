49146a96913778f8c54379a5c1b3f47192244fe6
Aaron <aaronlopezsosa@gmail.com>
2015-11-22 13:41:55 -0600

    parties ver

---
+++ cliente_party4u/protected/views/parties/ver.php
@@ -0,0 +1,42 @@
+<div class="box-row">
+	<div class="box-cell">
+		<div class="box-inner padding">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="col-xs-12">
+						<div class = "panel panel-card">
+							<div class = "item">
+								<img src = "../images/c1.jpg" class = "w-full r-t" />
+								<div class = "bottom text-white p"></div>
+							</div>
+							<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
+								<span class = "text-white">23</span>
+							</a>-->
+							<div class = "p">
+								<h3>XTREME POOL PARTY</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+								</p>	
+							</div>
+							<div class = "p">
+								<p>Ha juntado
+									<div class = "progress">
+										<div class = "progress-bar progress-bar-info" style="width:30%">Ha juntado $800,00 MXN</div>
+									</div>
+								</p>
+								<p class = "pull-left">Necesita:<br/>$1200,00 MXN</p>
+								<p class = "pull-right">Quedan:<br/>18 días</p>		
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
