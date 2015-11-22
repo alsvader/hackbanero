4f2911b171cb7ae1a192ccb762cf0231b4851111
Aaron <aaronlopezsosa@gmail.com>
2015-11-22 09:46:36 -0600

    lista de fiestas

--- cliente_party4u/protected/views/parties/index.php
+++ cliente_party4u/protected/views/parties/index.php
@@ -1,59 +1,94 @@
-<h1>Fiestas</h1>
+<h1 class = "text-center">Fiestas</h1>
 
 <div class = "row">
-	<div class = "col-sm-8">
+	<div class = "col-xs-10 col-xs-offset-1">
 		<div class = "md-list md-whiteframe-z0 bg-white m-b">
 			<div class = "md-list-item">
-				<div class = "md-list-item-left">
-					<img src = "">
-				</div>
+				<div class="rounded w-64 bg-white pull-left">
+                  <img src="../images/a0.jpg" class="img-responsive rounded">
+                </div>
 				<div class = "md-list-item-content">
-					<h3 class = "text-md">Nombre fiesta</h3>
+					<h3 class = "text-md">Extreme pool party</h3>
 					<small class = "font-thin">alsvader</small>
+					<p>Necesita: $3000.00</p>
+					<p>Monto recaudado: $3000.00</p>
+					<div class = "progress">
+						<div class = "progress-bar progress-bar-info" style="width:30%">30%</div>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.....</p>
+					<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
 				</div>
 			</div>
 		</div>
 		<div class = "md-list md-whiteframe-z0 bg-white m-b">
 			<div class = "md-list-item">
-				<div class = "md-list-item-left">
-					<img src = "">
-				</div>
+				<div class="rounded w-64 bg-white pull-left">
+                  <img src="../images/a0.jpg" class="img-responsive rounded">
+                </div>
 				<div class = "md-list-item-content">
-					<h3 class = "text-md">Nombre fiesta</h3>
+					<h3 class = "text-md">Aniversario cool en la pool</h3>
 					<small class = "font-thin">alsvader</small>
+					<p>Necesita: $3000.00</p>
+					<p>Monto recaudado: $3000.00</p>
+					<div class = "progress">
+						<div class = "progress-bar progress-bar-info" style="width:90%">90%</div>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.....</p>
+					<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
 				</div>
 			</div>
 		</div>
 		<div class = "md-list md-whiteframe-z0 bg-white m-b">
 			<div class = "md-list-item">
-				<div class = "md-list-item-left">
-					<img src = "">
-				</div>
+				<div class="rounded w-64 bg-white pull-left">
+                  <img src="../images/a0.jpg" class="img-responsive rounded">
+                </div>
 				<div class = "md-list-item-content">
-					<h3 class = "text-md">Nombre fiesta</h3>
+					<h3 class = "text-md">Escalofríos night</h3>
 					<small class = "font-thin">alsvader</small>
+					<p>Necesita: $3000.00</p>
+					<p>Monto recaudado: $3000.00</p>
+					<div class = "progress">
+						<div class = "progress-bar progress-bar-info" style="width:70%">70%</div>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.....</p>
+					<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
 				</div>
 			</div>
 		</div>
 		<div class = "md-list md-whiteframe-z0 bg-white m-b">
 			<div class = "md-list-item">
-				<div class = "md-list-item-left">
-					<img src = "">
-				</div>
+				<div class="rounded w-64 bg-white pull-left">
+                  <img src="../images/a0.jpg" class="img-responsive rounded">
+                </div>
 				<div class = "md-list-item-content">
-					<h3 class = "text-md">Nombre fiesta</h3>
+					<h3 class = "text-md">The cool zombie party</h3>
 					<small class = "font-thin">alsvader</small>
+					<p>Necesita: $3000.00</p>
+					<p>Monto recaudado: $3000.00</p>
+					<div class = "progress">
+						<div class = "progress-bar progress-bar-info" style="width:50%">50%</div>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.....</p>
+					<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
 				</div>
 			</div>
 		</div>
 		<div class = "md-list md-whiteframe-z0 bg-white m-b">
 			<div class = "md-list-item">
-				<div class = "md-list-item-left">
-					<img src = "">
-				</div>
+				<div class="rounded w-64 bg-white pull-left">
+                  <img src="../images/a0.jpg" class="img-responsive rounded">
+                </div>
 				<div class = "md-list-item-content">
-					<h3 class = "text-md">Nombre fiesta</h3>
+					<h3 class = "text-md">Jason's ultimate party</h3>
 					<small class = "font-thin">alsvader</small>
+					<p>Necesita: $3000.00</p>
+					<p>Monto recaudado: $3000.00</p>
+					<div class = "progress">
+						<div class = "progress-bar progress-bar-info" style="width:60%">60%</div>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.....</p>
+					<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
 				</div>
 			</div>
 		</div>
