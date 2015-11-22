b8af127beecef1590fe921cfa222e209059d7ece
Aaron <aaronlopezsosa@gmail.com>
2015-11-22 11:05:35 -0600

    bug corregido tamaño lista

--- cliente_party4u/protected/views/seguidores/index.php
+++ cliente_party4u/protected/views/seguidores/index.php
@@ -1,93 +1,112 @@
 <h1 class = "text-center">Seguidores</h1>
-<div class = "row">
-	<div class = "col-xs-10 col-xs-offset-1">
-		<div class = "panel panel-card">
-			<div class = "item">
-				<img src = "../images/c1.jpg" class = "w-full r-t" />
-				<div class = "bottom text-white p">Usuario</div>
-			</div>
-			<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
-				<span class = "text-white">23</span>
-			</a>-->
-			<div class = "p">
-				<h3>alsvader</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua...
-				<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
-				</p>
-			</div>
-		</div>
-	</div>
-	<div class = "col-xs-10 col-xs-offset-1">
-		<div class = "panel panel-card">
-			<div class = "item">
-				<img src = "../images/c1.jpg" class = "w-full r-t" />
-				<div class = "bottom text-white p">Usuario</div>
-			</div>
-			<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
-				<span class = "text-white">23</span>
-			</a>-->
-			<div class = "p">
-				<h3>alsvader</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua...
-				<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
-				</p>
-			</div>
-		</div>
-	</div>
-	<div class = "col-xs-10 col-xs-offset-1">
-		<div class = "panel panel-card">
-			<div class = "item">
-				<img src = "../images/c1.jpg" class = "w-full r-t" />
-				<div class = "bottom text-white p">Usuario</div>
-			</div>
-			<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
-				<span class = "text-white">23</span>
-			</a>-->
-			<div class = "p">
-				<h3>alsvader</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua...
-				<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
-				</p>
-			</div>
-		</div>
-	</div>
-	<div class = "col-xs-10 col-xs-offset-1">
-		<div class = "panel panel-card">
-			<div class = "item">
-				<img src = "../images/c1.jpg" class = "w-full r-t" />
-				<div class = "bottom text-white p">Usuario</div>
-			</div>
-			<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
-				<span class = "text-white">23</span>
-			</a>-->
-			<div class = "p">
-				<h3>alsvader</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua...
-				<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
-				</p>
-			</div>
-		</div>
-	</div>
-	<div class = "col-xs-10 col-xs-offset-1">
-		<div class = "panel panel-card">
-			<div class = "item">
-				<img src = "../images/c1.jpg" class = "w-full r-t" />
-				<div class = "bottom text-white p">Usuario</div>
-			</div>
-			<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
-				<span class = "text-white">23</span>
-			</a>-->
-			<div class = "p">
-				<h3>alsvader</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua...
-				<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
-				</p>
+
+<div class="box-row">
+	<div class="box-cell">
+		<div class="box-inner padding">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="col-xs-12">
+						<div class = "panel panel-card">
+							<div class = "item">
+								<img src = "../images/c1.jpg" class = "w-full r-t" />
+								<div class = "bottom text-white p">Usuario</div>
+							</div>
+							<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
+								<span class = "text-white">23</span>
+							</a>-->
+							<div class = "p">
+								<h3>alsvader</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua...
+								<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12">
+					<div class="col-xs-12">
+						<div class = "panel panel-card">
+							<div class = "item">
+								<img src = "../images/c1.jpg" class = "w-full r-t" />
+								<div class = "bottom text-white p">Usuario</div>
+							</div>
+							<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
+								<span class = "text-white">23</span>
+							</a>-->
+							<div class = "p">
+								<h3>alsvader</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua...
+								<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12">
+					<div class="col-xs-12">
+						<div class = "panel panel-card">
+							<div class = "item">
+								<img src = "../images/c1.jpg" class = "w-full r-t" />
+								<div class = "bottom text-white p">Usuario</div>
+							</div>
+							<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
+								<span class = "text-white">23</span>
+							</a>-->
+							<div class = "p">
+								<h3>alsvader</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua...
+								<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12">
+					<div class="col-xs-12">
+						<div class = "panel panel-card">
+							<div class = "item">
+								<img src = "../images/c1.jpg" class = "w-full r-t" />
+								<div class = "bottom text-white p">Usuario</div>
+							</div>
+							<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
+								<span class = "text-white">23</span>
+							</a>-->
+							<div class = "p">
+								<h3>alsvader</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua...
+								<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12">
+					<div class="col-xs-12">
+						<div class = "panel panel-card">
+							<div class = "item">
+								<img src = "../images/c1.jpg" class = "w-full r-t" />
+								<div class = "bottom text-white p">Usuario</div>
+							</div>
+							<!--<a class="md-btn md-raised md-fab brown m-r md-fab-offset pull-right waves-effect" md-ink-ripple="">
+								<span class = "text-white">23</span>
+							</a>-->
+							<div class = "p">
+								<h3>alsvader</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua...
+								<button class = "btn btn-rounded btn-stroke btn-info waves-effect pull-right">Ver</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				
 			</div>
 		</div>
 	</div>
 </div>
+
