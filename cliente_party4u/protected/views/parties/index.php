824c95945e96b7bb83bec776a98fcfdcd9bbd51e
Aaron <aaronlopezsosa@gmail.com>
2015-11-22 09:03:23 -0600

    cambios vista parties

--- cliente_party4u/protected/views/parties/index.php
+++ cliente_party4u/protected/views/parties/index.php
@@ -1,36 +1,61 @@
 <h1>Fiestas</h1>
 
-<div class="box-body" id="contenido"></div>
-
-<a href="<?php echo Yii::app()->createUrl('parties/create');?>" class="md-btn md-fab md-fab-bottom-right pos-fix teal">
-	<i class="mdi-editor-mode-edit i-24"></i>
-</a>
-
-<?php
-Yii::app()->clientScript->registerScript(
-	'scritpt-contenido-table',
-	'$.ajax({
-		url: "http://'.$this->url_server.'/hackbanero/server_party4u/fiesta/index.html",
-		type: "post",
-		dataType: "jsonp",
-		data: {user_id:'.Yii::app()->user->id.'},
-		success: function(data){
-			$("#contenido").empty();
-			if(data.data.fiestas.length > 0){
-				$("#content_list").show();
-				$.post(
-					"http://'.$this->url_server.'/hackbanero/cliente_party4u/parties/index.html",
-					{data:data.data},
-					function(data){
-						$("#contenido").html(data);
-					}
-				);
-			}else{
-				$("#content_vacio").show();
-				$("#content_list").hide();
-			}
-		}
-	});',
-	CClientScript::POS_READY
-);
-?>
+<div class = "row">
+	<div class = "col-sm-8">
+		<div class = "md-list md-whiteframe-z0 bg-white m-b">
+			<div class = "md-list-item">
+				<div class = "md-list-item-left">
+					<img src = "">
+				</div>
+				<div class = "md-list-item-content">
+					<h3 class = "text-md">Nombre fiesta</h3>
+					<small class = "font-thin">alsvader</small>
+				</div>
+			</div>
+		</div>
+		<div class = "md-list md-whiteframe-z0 bg-white m-b">
+			<div class = "md-list-item">
+				<div class = "md-list-item-left">
+					<img src = "">
+				</div>
+				<div class = "md-list-item-content">
+					<h3 class = "text-md">Nombre fiesta</h3>
+					<small class = "font-thin">alsvader</small>
+				</div>
+			</div>
+		</div>
+		<div class = "md-list md-whiteframe-z0 bg-white m-b">
+			<div class = "md-list-item">
+				<div class = "md-list-item-left">
+					<img src = "">
+				</div>
+				<div class = "md-list-item-content">
+					<h3 class = "text-md">Nombre fiesta</h3>
+					<small class = "font-thin">alsvader</small>
+				</div>
+			</div>
+		</div>
+		<div class = "md-list md-whiteframe-z0 bg-white m-b">
+			<div class = "md-list-item">
+				<div class = "md-list-item-left">
+					<img src = "">
+				</div>
+				<div class = "md-list-item-content">
+					<h3 class = "text-md">Nombre fiesta</h3>
+					<small class = "font-thin">alsvader</small>
+				</div>
+			</div>
+		</div>
+		<div class = "md-list md-whiteframe-z0 bg-white m-b">
+			<div class = "md-list-item">
+				<div class = "md-list-item-left">
+					<img src = "">
+				</div>
+				<div class = "md-list-item-content">
+					<h3 class = "text-md">Nombre fiesta</h3>
+					<small class = "font-thin">alsvader</small>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
